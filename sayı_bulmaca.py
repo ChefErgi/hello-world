c1ae0e2523bf4a4edada3fc3503f1a998cc05999
ChefErgi <74680943+ChefErgi@users.noreply.github.com>
2020-11-18 23:02:28 +0300

    Add files via upload

---
+++ sayı_bulmaca.py
@@ -0,0 +1,28 @@
+# 1. Bilgisayyar 1 - 100 arasında bir sayı tutar
+# 2. Kullanıcı bu sayıyı tahmin eder
+# 3. Eğer girilen sayı tutulan sayıdan BÜYÜKSE ekrana Aşağı yazar
+# 4. Eğer girilen sayı tutulan sayıdan  KÜÇÜKSE ekrana YUKARI yazar
+# 5. Eğer girilen sayı tutulan sayıya eşit ise ekrana BİLDİN yazar
+import random
+
+try:
+    rasgtele_sayi = random.randint(1, 100)
+    print(rasgtele_sayi)
+    count = 0
+    sayı_bilindi_mi = False
+    while sayı_bilindi_mi == False:
+        tahmin = int( input("1-100 arasındai sayıyı tahmin ediniz: "))
+        count += 1
+        if tahmin < rasgtele_sayi:
+            print("Yukarı, tekrar deneyiniz.")
+        elif tahmin > rasgtele_sayi:
+            print("Aşağı, tekrar deneyiniz.")
+        else:
+            print("Vallahi doğru bildiniz.")
+            print("Sayıyı {} denemede bildiniz".format(count))
+            sayı_bilindi_mi = True
+
+except ValueError:
+    print("Lütfen tamsayı yazınız.")
+
+#döngü: belirli bir koşul gerçekleşinceye dek tekrar işlem
